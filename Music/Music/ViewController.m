9745be6c22302c1187165e7dee07fa5098cf4928
ABC_Show <361569679@qq.com>
2016-11-07 16:13:37 +0800

    简单实现音频播放

--- Music/Music/ViewController.m
+++ Music/Music/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+// 导入音视频的类库
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
+
+
 
 @end
 
@@ -18,6 +22,23 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"buyao.caf" ofType:nil];
+    NSURL *url = [NSURL fileURLWithPath:filePath];
+    
+    SystemSoundID systemSound;
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef _Nonnull)(url), &systemSound);
+    // 播放
+    AudioServicesPlaySystemSound(systemSound);
+    
+}
+
+    
+    
+    
+
 
 
 - (void)didReceiveMemoryWarning {
